533473ccb269f02880b8e8c41e9c6a03b2002af7
anupamit <anupamit011@gmail.com>
2024-12-04 12:56:32 +0530

    space corrected

--- src/pageComponent/SidebarPage.tsx
+++ src/pageComponent/SidebarPage.tsx
@@ -360,7 +360,7 @@ export default function SidebarPage() {
               </Breadcrumb>
             </div>
           </header>
-          <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          <div className="flex flex-1 flex-col p-1 pt-0">
             <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
               <Outlet />
             </div>
--- src/pageComponent/UploadDoc.tsx
+++ src/pageComponent/UploadDoc.tsx
@@ -27,7 +27,7 @@ export default function UploadDoc() {
   };
 
   return (
-    <div className="container mx-auto p-6 space-y-8">
+    <div className="container mx-auto p-2 space-y-2">
       <h1 className="text-2xl font-bold text-primary">Document Upload</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         <Card className="bg-white shadow-lg">
